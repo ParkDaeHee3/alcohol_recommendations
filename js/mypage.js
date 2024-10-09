aaa2c441a0a54f6faa8411d16702ef5d1018ffb1
LEE CHUNG HYUN <dlcndgus54@gmail.com>
2024-10-09 17:51:22 +0900

    mypage_update

---
+++ js/mypage.js
@@ -0,0 +1,16 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+}
+
+function openSearch() {
+    alert('검색 기능이 활성화되었습니다.');
+}
+
+function editInfo() {
+    alert('정보 수정 페이지로 이동합니다.');
+}
+
+function submitInquiry() {
+    alert('문의가 제출되었습니다.');
+}
